8440ad37531c0cca9033ec528a86947663fec3b8
DaoBaoKha <daobaokha2509@gmail.com>
2025-03-02 00:15:11 +0700

    UpdateBackground

---
+++ Assets/GangsterHealthBar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class GangsterHealthBar : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    public Image healthBar;
+
+    void Start(){
+        maxHealth = health;
+    }
+
+    void Update(){
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+        
+         if (health < 0) {
+        Destroy(gameObject);
+    }
+    }
+}
